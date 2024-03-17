abb71369ca35ea9192c764829d9dfcb801a29869
shivcodecf <shivam2113128@akgec.ac.in>
2024-03-18 00:44:01 +0530

    changes

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useContext } from "react";
+import { useContext ,useRef } from "react";
 import {useState}from 'react'
 import { Questions } from './helpers/Questions'
 import { GameStateContext } from './helpers/contests'
@@ -8,26 +8,60 @@ import './style.css';
 
 const Quiz = () => { 
     const [optionChosen, setOptionChosen] = useState("");
-    const[index,setindex]= useState(0);
+    const[index,setindex]= useState(0);  
+    const[lock,setlock]=useState(false);
+    const boxRef = useRef(null); // Create a ref
     const { score, setScore, gameState, setGameState } = useContext(
         GameStateContext
       );
    
-      const chooseOption = (option) => {
-        setOptionChosen(option);
-      }; 
+      const chooseOption = (option,e) => {
+       
+         
+        if(lock==false){  
+          setOptionChosen(option); 
+         
+        if (Questions[index].asnwer === option) {
+          e.target.classList.add('correct');
+          setScore(score + 1); 
+          setlock(true);
+        } else {
+          e.target.classList.add('incorrect');
+          setlock(true);
+        }
+      }
+      
+        
+       
+       
+       
+      };  
+
+
+
+      const checkAnswer = () => {
+        
+    };
+
+
 
       const finishQuiz = () => {
-        if (Questions[index].asnwer == optionChosen) {
-          setScore(score + 1);
-        }
+       
+       
         setGameState("stop");
+       
+        
       };
-      const nextQuestion = () => {
-        if (Questions[index].asnwer == optionChosen) {
-          setScore(score + 1);
-        }
+      const nextQuestion = () => { 
+        
+        // if (Questions[index].asnwer == optionChosen) {
+        //   setScore(score + 1);
+        
+        // } 
+
         setindex(index + 1);
+        setOptionChosen(""); 
+        setlock(false);
       };
     
 
@@ -38,31 +72,68 @@ const Quiz = () => {
       
         <h3>{Questions[index].prompt}</h3> 
         <div className="questions">
-        <button 
-          onClick={() => {
-            chooseOption("optionA");
+        <button  
+          className={
+            optionChosen === "optionA" && Questions[index].asnwer === "optionA" ? "correct" :
+            optionChosen === "optionA" && Questions[index].asnwer !== "optionA" ? "incorrect" :
+            "black" }
+       
+          onClick={(e) => {
+            {chooseOption("optionA",e)};
+            checkAnswer()
+           
+
           }}
+         
         >
           {Questions[index].optionA}
         </button>
-        <button
-          onClick={() => {
-            chooseOption("optionB");
+        <button  
+          className={
+            optionChosen === "optionB" && Questions[index].asnwer === "optionB" ? "correct" :
+            optionChosen === "optionB" && Questions[index].asnwer !== "optionB" ? "incorrect" :
+            "black" }
+       
+          onClick={(e) => {
+            chooseOption("optionB",e);
+            checkAnswer()
+           
+
           }}
+        
         >
           {Questions[index].optionB}
         </button>
-        <button
-          onClick={() => {
-            chooseOption("optionC");
+        <button 
+         className={
+          optionChosen === "optionC" && Questions[index].asnwer === "optionC" ? "correct" :
+          optionChosen === "optionC" && Questions[index].asnwer !== "optionC" ? "incorrect" :
+          "black"
+      }
+      
+         
+          onClick={(e) => {
+            chooseOption("optionC",e);
+            checkAnswer()
+
           }}
+         
         >
           {Questions[index].optionC}
-        </button>
-        <button
-          onClick={() => {
-            chooseOption("optionD");
+        </button> 
+        <button 
+           className={
+            optionChosen === "optionD" && Questions[index].asnwer === "optionD" ? "correct" :
+            optionChosen === "optionD" && Questions[index].asnwer !== "optionD" ? "incorrect" :
+            "black"
+        }
+         
+          onClick={(e) => {
+            chooseOption("optionD",e);
+            checkAnswer()
+
           }}
+         
         >
           {Questions[index].optionD}
         </button>
@@ -85,4 +156,4 @@ const Quiz = () => {
   )
 }
 
-export default Quiz
+export default Quiz  
